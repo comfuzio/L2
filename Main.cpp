2eeb48c3671f6befb0df8f7d71fbf635be8ac64a
George T <30821243+comfuzio@users.noreply.github.com>
2022-03-20 11:56:21 +0200

    Update Main.cpp
    Changing the ip to Lineage2World's IP

--- Main.cpp
+++ Main.cpp
@@ -9,5 +9,5 @@
 // You can change the path to the .bin file to avoid including the L2.exe inside the System folder.
 // Author: Zoey76
 int _stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
-	ShellExecute(0, L"open", L"cmd.exe", L"/C start l2.bin IP=127.0.0.1", 0, SW_HIDE);
+	ShellExecute(0, L"open", L"cmd.exe", L"/C start l2.bin IP=auth.lineage2world.eu", 0, SW_HIDE);
 }
